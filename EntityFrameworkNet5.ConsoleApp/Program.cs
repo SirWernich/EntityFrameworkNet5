36b69b563e2bc28912732628af93657a8f726fbd
Wernich Wilsenach <wernich@chasesoftware.co.za>
2022-04-24 16:28:18 +0200

    Initial commit
    Set up solution.

---
+++ EntityFrameworkNet5.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EntityFrameworkNet5.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
