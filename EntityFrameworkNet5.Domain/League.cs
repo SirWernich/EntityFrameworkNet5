2f3ff4bc87d6383f26f68f0ab70192f0f08086a3
Wernich Wilsenach <melktert@gmail.com>
2022-06-23 19:21:00 +0200

    Add models for tables

---
+++ EntityFrameworkNet5.Domain/League.cs
@@ -0,0 +1,9 @@
+namespace EntityFrameworkNet5.Domain
+{
+    public class League
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ EntityFrameworkNet5.Domain/Team.cs
@@ -0,0 +1,13 @@
+namespace EntityFrameworkNet5.Domain
+{
+    public class Team
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        
+        public int LeagueId { get; set; }
+
+        public virtual League League { get; set; }
+    }
+}
