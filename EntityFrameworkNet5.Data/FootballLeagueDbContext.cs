2696bdc9d559ad860cb09842b5ba7c6cda4dbf1a
Wernich Wilsenach <melktert@gmail.com>
2022-09-30 14:47:57 +0200

    Enable query logging, change to dotnet6
    Enable query logging.
    Change project to dotnet6.

--- EntityFrameworkNet5.Data/FootballLeagueDbContext.cs
+++ EntityFrameworkNet5.Data/FootballLeagueDbContext.cs
@@ -1,4 +1,6 @@
+using System;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 using EntityFrameworkNet5.Domain;
 
@@ -8,7 +10,9 @@ namespace EntityFrameworkNet5.Data
     {
         protected override void OnConfiguring( DbContextOptionsBuilder optionsBuilder )
         {
-            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=FootballLeague_EfCore;User Id=sa;Password=P@ssw0rd!");
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=FootballLeague_EfCore;User Id=sa;Password=P@ssw0rd!")
+                .LogTo(Console.WriteLine, new [] { DbLoggerCategory.Database.Command.Name }, LogLevel.Information )
+                .EnableSensitiveDataLogging();
         }
 
         public DbSet<Team> Teams { get; set; }
